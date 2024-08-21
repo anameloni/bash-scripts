10c89f7f9d4469a010635ee1c97febcf0ec40679
Ana Luiza Meloni Dias <45142965+anameloni@users.noreply.github.com>
2024-08-21 13:04:53 +0100

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update apt cache.
-sudo apt-get update
+sudo apt update
 
 # Install Nginx.
-sudo apt-get install nginx -y 
+sudo apt install nginx -y 
 
 # Set the home page.
 echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
